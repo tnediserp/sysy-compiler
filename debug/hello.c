033ad1786aa1caa12174fb1c717e480974e6930e
tnediserp <1211406360@qq.com>
2024-04-06 16:55:25 +0800

    lv1: fix a problem on comments

--- debug/hello.c
+++ debug/hello.c
@@ -1,8 +1,14 @@
+/* this is very good*/
 int main ()
 {
     // this is a comment 
-    /*this is a new line.
-
-    */
-    return 100;
+    /* this is 
+    
+    
+    a new line.*/
+    return 100
+    ;
+    /* this is another comment */
 }
+/* this is bad*/
+/*yes*/
